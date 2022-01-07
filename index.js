555f0ff52e13717999ceb6fbf18142337abb3148
amarten <alexdmarten@gmail.com>
2022-01-06 21:42:37 -0800

    all prompts added with inquirer

--- index.js
+++ index.js
@@ -6,11 +6,112 @@ const promptUser = () => {
     return inquirer.prompt([
         {
             type: 'input',
+            name: 'username',
+            message: 'What is your GitHub Username? (Required)',
+            validate: usernameInput => {
+                if(usernameInput) {
+                    return true;
+                } else {
+                    console.log("Please enter your GitHub username!")
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email address? (Required)',
+            validate: emailInput => {
+                if(emailInput) {
+                    return true;
+                } else {
+                    console.log("Please enter your email address!");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
             name: 'title',
-            message: 'What is the title of your project?'
+            message: 'What is the title of your project? (Required)',
+            validate: titleInput => {
+                if(titleInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your project title!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'What is the description of your project? (Required)',
+            validate: descriptionInput => {
+                if(descriptionInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your project description!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'What is the installation procedure of your project? (Required)',
+            validate: installationInput => {
+                if(installationInput) {
+                    return true;
+                } else {
+                    console.log("Please provide installation instructions!");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'What is the intended use of your project?',
+            validate: usageInput => {
+                if(usageInput) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'contributors',
+            message: 'List any contributors to your project, if none then leave blank and press enter.'
+        },
+        {
+            type: 'input',
+            name: 'testing',
+            message: 'If your project has tests, describe them and their intended use here. If none then leave blank and press enter.'
+        },
+        {
+            type: 'checkbox',
+            name: 'license',
+            message: 'Please select any license that applies to your project. If none apply, select None. (Required)',
+            choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0','The Unlicense', 'None'],
+            validate: licenseInput => {
+                if(licenseInput) {
+                    return true;
+                } else {
+                    console.log('Please select an option.')
+                    return false;
+                }
+            }
         }
     ])
 }
+
+promptUser()
+    .then(answers => {
+        console.log(answers)
+    })
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
